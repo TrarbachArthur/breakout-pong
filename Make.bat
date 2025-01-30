7baf94ea8d9d96d441478f75f1b9f2ceec31699c
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2025-01-29 22:10:02 -0300

    Add given starter files

---
+++ Make.bat
@@ -0,0 +1,8 @@
+call nasm main 
+call nasm line 
+call nasm fcircle 
+call nasm circle 
+call nasm cursor 
+call nasm caracter 
+call nasm plot_xy
+call freelink main line fcircle circle cursor caracter plot_xy
